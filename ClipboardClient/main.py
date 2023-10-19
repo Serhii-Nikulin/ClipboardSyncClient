ca695ca065b7db5a33eea013fd30f7cdf2355900
Serhii-Nikulin <sergey.nikulin99@gmail.com>
2023-10-19 20:58:52 +0300

    1.Start

--- ClipboardClient/main.py
+++ ClipboardClient/main.py
@@ -1,11 +1,15 @@
 import clipboard
 from loguru import logger
 
-clipboard.copy("abcasdfsaf qw r31qr qw f\n"
-               "asdf asdf as "
-               "as dfq3et1345 "
-               "34t 413"
-               " ")  # now the clipboard content will be string "abc"
-text = clipboard.paste()  # text will have the content of clipboard
-if len(text) > 20:
-    logger.critical('Syka blyadliviy text nahuy pidor');
+import time
+
+text = clipboard.paste()
+
+while True:
+    time.sleep(1)
+    new_text = clipboard.paste()
+    if text != new_text:
+        text = new_text
+        logger.debug(text)
+
+
